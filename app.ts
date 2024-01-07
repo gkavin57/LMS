b8b731d523034ca88b2b73288cd3998c47e23bd9
gkavin57 <gkavin77@gmail.com>
2024-01-07 18:32:55 +0530

    initialCommit

---
+++ app.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response, NextFunction } from "express";
+require("dotenv").config();
+export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//body parser-for cloudinery
+app.use(express.json({ limit: "50mb" }));
+
+//cookie-parser
+app.use(cookieParser());
+
+//cors
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+//testing api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working",
+  });
+});
+
+//unknown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`route ${req.originalUrl} not found`);
+  next(err);
+});
